0b2273443712400fb9c212d7106f57b00d366ea1
Aditya Naik <91489422+adkian-sifive@users.noreply.github.com>
2026-05-04 18:33:04 -0700

    [Scala3] Add Definition/Instance API (#5277)

--- core/src/main/scala-3/chisel3/experimental/hierarchy/IsA.scala
+++ core/src/main/scala-3/chisel3/experimental/hierarchy/IsA.scala
@@ -1,6 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package chisel3.experimental.hierarchy.core
 
 import chisel3._
+import chisel3.experimental.hierarchy.{hierarchyLookupAux, HierarchyLookupMacro}
+import scala.reflect.ClassTag
 
 trait HierarchyProto[+A] {
   private[chisel3] def underlying: Underlying[A]
@@ -8,12 +12,34 @@ trait HierarchyProto[+A] {
     case Proto(value)         => value
     case Clone(i: IsClone[A]) => i.getProto
   }
+
+  // The transparent inline here ensures the precise return type
+  // propagates to the call site
+  transparent inline def selectDynamic(inline name: String): Any = {
+    ${
+      HierarchyLookupMacro.selectDynamicImpl[A](
+        '{ this.asInstanceOf[Hierarchy[A]] },
+        'name
+      )
+    }
+  }
+
+  transparent inline def applyDynamic(inline name: String)(inline args: Any*): Any = {
+    ${
+      HierarchyLookupMacro.applyDynamicImpl[A](
+        '{ this.asInstanceOf[Hierarchy[A]] },
+        'name,
+        'args
+      )
+    }
+  }
 }
 
-trait HierarchyIsA[+A] extends HierarchyProto[A] {
-  // This code handles a special-case where, within an mdoc context, the type returned from
-  //  scala reflection (typetag) looks different than when returned from java reflection.
-  //  This function detects this case and reshapes the string to match.
+trait HierarchyIsA[+A] extends HierarchyProto[A] with scala.Selectable {
+  // This code handles a special-case where, within an mdoc context,
+  //  the type returned from scala reflection (typetag) looks
+  //  different than when returned from java reflection.  This
+  //  function detects this case and reshapes the string to match.
   private def modifyReplString(clz: String): String = {
     if (clz != null) {
       clz.split('.').toList match {
@@ -37,12 +63,19 @@ trait HierarchyIsA[+A] extends HierarchyProto[A] {
 
   /** Determine whether underlying proto is of type provided.
     *
-    * @note IMPORTANT: this function requires summoning a TypeTag[B], which will fail if B is an inner class.
-    * @note IMPORTANT: this function IGNORES type parameters, akin to normal type erasure.
-    * @note IMPORTANT: this function relies on Java reflection for underlying proto, but Scala reflection for provided type
+    * @note IMPORTANT: this function IGNORES type parameters, akin to
+    * normal type erasure.
+    * @note IMPORTANT: this function relies on Java reflection for
+    * underlying proto, but Scala reflection for provided type
     *
-    * E.g. isA[List[Int]] will return true, even if underlying proto is of type List[String]
-    * @return Whether underlying proto is of provided type (with caveats outlined above)
+    * E.g. isA[List[Int]] will return true, even if underlying proto
+    * is of type List[String]
+    * @return Whether underlying proto is of provided type (with
+    * caveats outlined above)
     */
-  def isA[B]: Boolean = false // TODO
+  def isA[B: ClassTag]: Boolean = {
+    val clzName = summon[ClassTag[B]].runtimeClass.getCanonicalName
+    if (clzName == null) false
+    else inBaseClasses(modifyReplString(clzName))
+  }
 }
